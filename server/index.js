f119842745f7d88794845a614c80efa998e1ec55
DESKTOP-IIGJESU\dotan <dotanluc1301@gmail.com>
2022-01-12 22:18:23 +0700

    adding config to serve mime type

--- server/index.js
+++ server/index.js
@@ -11,6 +11,8 @@ const app = express();
 // adding Helmet to enhance your API's security
 app.use(helmet());
 
+app.use(express.static(__dirname + '/static'));
+
 // using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
